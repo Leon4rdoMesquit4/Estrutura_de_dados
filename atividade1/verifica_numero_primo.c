9ea1776342d8f2979d6e2420c08689d779d19aca
Leonardo Mesquita Alves <leonardo.pro.alves@gmail.com>
2023-08-13 01:55:14 -0300

    Alterações adicionais para casos que poderiam dar erro

--- atividade1/verifica_numero_primo.c
+++ atividade1/verifica_numero_primo.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void isPrimeNumber(int number){
     int isFinded = 0;
-
-     for (int i = 2; i < number; i++){
-        int resultOfDivider = number % i;
-        if (resultOfDivider == 0){
-            printf("0");
-            isFinded++;
-            break;
+    if (number < 2){
+        printf("0");
+    } else{
+        for (int i = 2; i < number; i++){
+            int resultOfDivider = number % i;
+            if (resultOfDivider == 0){
+                printf("0");
+                isFinded++;
+                break;
+            }
         }
-     }
 
-    if(isFinded == 0){
-        printf("1");
+        if(isFinded == 0){
+            printf("1");
+        }
     }
+     
 };
 
 int main(int argc, char *argv[]){
-    int pop = 34327;
-    isPrimeNumber(pop);
+    if (atoi(argv[1])){
+        isPrimeNumber(atoi(argv[1]));
+    } else {
+        printf("Error, something is wrong");
+    }
 };
