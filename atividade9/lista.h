1512e5b75273f9ad7581595ab439b173fe3a123f
Leonardo Mesquita Alves <leonardo.pro.alves@gmail.com>
2023-10-22 23:11:45 -0300

    Atividade9

--- atividade9/lista.h
+++ atividade9/lista.h
@@ -1,4 +1,4 @@
-typedef struct No{
+typedef struct No {
     float valor;
     struct No* proximo_no;
 } No;
@@ -7,7 +7,7 @@ No* no(float valor, No* proximo_no);
 void lista_inserir_no(No* L, No* no);
 void lista_imprimir(No* L);
 int lista_quantidade_nos(No* L);
-No* lista_copiar(No* L);
-void lista_concatenar(No* L, No* Lc);
 void lista_liberar(No* L);
+
+
 void lista_inserir_no_ordenado(No** L, No* no);
--- atividade9/main.c
+++ atividade9/main.c
@@ -1,29 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "lista.h"
 
+int main(int argc, char *argv[])
+{
+    clock_t inicio, final;
+    double cpu_time;
 
-int main(int argc, char* argv[]){
+    No *cabeca = NULL;
+    FILE *arquivo = fopen("1kk_rand_float.csv", "r");
 
-    FILE *file;
-    file = fopen("1kk_rand_float.csv", "r");
-
-    if (file == NULL){
-        printf("Erro de leitura do arquivo");
-        exit(1);
+    if (arquivo == NULL)
+    {
+        printf("Erro na leitura\n");
+        return 1;
     }
 
-    No* naoOrdenada = NULL;
-    No* ordenada = NULL;
+    int tamanho = 1000000;
+    float numero;
 
-    int tamanho = 1001;
-    float numbers;
-    int i = 0;
-    while(fscanf(file, "%f\n", &numbers) == 1 && i < tamanho){
-        i++;
+    inicio = clock();
+    while (fscanf(arquivo, "%f", &numero) != EOF)
+    {
+        No *noTeste = (No *)malloc(sizeof(No));
+
+        if (noTeste == NULL)
+        {
+            printf("Erro ao alocar memória para os números.\n");
+            fclose(arquivo);
+            return 1;
+        }
+
+        noTeste->valor = numero;
+        noTeste->proximo_no = cabeca;
+        cabeca = noTeste;
     }
 
-    fclose(file);
+    final = clock();
+    cpu_time = ((double)(final - inicio)) / CLOCKS_PER_SEC;
+    printf("tempo para inserir no de forma desordenada: %f segundos\n", cpu_time);
 
-    exit(0);
+    fclose(arquivo);
+
+    cabeca = NULL; 
+
+    arquivo = fopen("1kk_rand_float.csv", "r");
+
+    inicio = clock();
+    while (fscanf(arquivo, "%f", &numero) != EOF)
+    {
+        No *noTeste = (No *)malloc(sizeof(No));
+
+        if (noTeste == NULL)
+        {
+            printf("Erro ao alocar memória para os números.\n");
+            fclose(arquivo);
+            return 1;
+        }
+
+        noTeste->valor = numero;
+        lista_inserir_no_ordenado(&cabeca, noTeste);
+    }
+
+    final = clock();
+    cpu_time = ((double)(final - inicio)) / CLOCKS_PER_SEC;
+    printf("tempo para inserir no de forma ordenada: %f segundos\n", cpu_time);
+
+    fclose(arquivo);
+
+    return 0;
 }
